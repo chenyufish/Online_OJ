d9a05e12d0b326a15b4c0c35b56243a70ecf8a1f
fishman <cy13692810010@163.com>
2024-05-02 19:05:07 +0800

    路径修改完毕，添加nacos注册中心，使用openfegin去发现，注意引入负载均衡

--- oj_backend_microservice/oj_backend_judge_service/src/main/java/com/fishman/oj_backend_judge_service/OjBackendJudgeServiceApplication.java
+++ oj_backend_microservice/oj_backend_judge_service/src/main/java/com/fishman/oj_backend_judge_service/OjBackendJudgeServiceApplication.java
@@ -2,6 +2,8 @@ package com.fishman.oj_backend_judge_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +12,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 @ComponentScan("com.fishman")
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.fishman.oj_backend_service_client.service"})
 public class OjBackendJudgeServiceApplication {
 
     public static void main(String[] args) {
